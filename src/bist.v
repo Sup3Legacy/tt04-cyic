fb361db1ee6782e9851715b235651ddc21a7d6ba
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2023-09-07 15:37:52 +0200

    Add BIST internal vector filling procedure

--- src/bist.v
+++ src/bist.v
@@ -9,6 +9,25 @@ module bist (
     output is_broken,
     output is_init
 );
+  reg [63:0] bitvec;
+  reg is_filling;
+  reg [7:0] filling_status;
+
+  initial begin
+    bitvec = 0;
+    is_filling = 1;
+    filling_status = 0;
+  end
+
+  always @ (posedge clk) begin
+    if (is_filling) begin
+      filling_status = filling_status + 1;
+      if (filling_status == 0) begin
+          is_filling = 0;
+      end
+    end
+    bitvec = (bitvec << 1) | { 63'b0, entropy_bit};
+  end
 
 endmodule
 
